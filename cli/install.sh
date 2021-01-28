4bdec28b28b77636a373c1777333f76c26f6717f
jannes <jannes.dev@posteo.net>
2021-01-28 22:16:21 +0100

    Fix make and install scripts
    The dictionary.txt was not loading due
    to the way it was packaged and also because
    the run script did not switch working dir.

--- cli/install.sh
+++ cli/install.sh
@@ -2,8 +2,9 @@
 # run as root
 # places a script on path that calls the executable in this directory's dist folder
 current_dir=$(pwd)
-executable_path="${current_dir}/dist/han-segmenter/han-segmenter"
+executable_dir="${current_dir}/dist/han-segmenter"
 echo "#!/bin/bash" > /usr/local/bin/han-segmenter
-echo "${executable_path} "'"$@"' >> /usr/local/bin/han-segmenter
+echo "cd ${executable_dir}">> /usr/local/bin/han-segmenter
+echo "./han-segmenter "'"$@"' >> /usr/local/bin/han-segmenter
 chmod +x /usr/local/bin/han-segmenter
 
