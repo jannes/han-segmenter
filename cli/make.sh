182d30096164312de5788c3fecfbdfa1d7f55b52
jannes <jannes.dev@posteo.net>
2022-06-23 20:41:44 +0200

    Fix: include new dictionary in pyinstaller build

--- cli/make.sh
+++ cli/make.sh
@@ -4,5 +4,6 @@ pyinstaller \
     --add-data "${site_packages_dir}/pkuseg/dicts:pkuseg/dicts" \
     --add-data "${site_packages_dir}/pkuseg/models:pkuseg/models" \
     --add-data "dictionary.txt:." \
+    --add-data "dictionary-fanjian.txt:." \
     --onedir main.py \
     --name han-segmenter
